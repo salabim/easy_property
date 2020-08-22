3317a4f7ed5ab34a07094bee33eba8a06f993ecc
salabim <30370571+salabim@users.noreply.github.com>
2020-08-22 15:16:01 +0200

    0.0.5

--- easy_property.py
+++ easy_property.py
@@ -70,20 +70,22 @@ Class Demo:
 """
 import functools
 
-__version__ = "0.0.2"
+__version__ = "0.0.5"
+
 
 def action(f, frefs):
     if f.__qualname__ == action.qualname:
-        if any (action.f[fref] is not None for fref in frefs.split("_")):
+        if any(action.f[fref] is not None for fref in frefs.split("_")):
             raise AttributeError(f"decorator defined twice")
     else:
         action.f.update({}.fromkeys(action.f, None))  # reset all values to None
         action.qualname = f.__qualname__
-    action.f.update({}.fromkeys(frefs.split('_'), f))  # set all frefs values to f
-    return property(*(action.f[ref] if (ref != "documenter" or action.f[ref] is None) else action.f[ref](0) for ref in action.f))
+    action.f.update({}.fromkeys(frefs.split("_"), f(0) if frefs == "documenter" else f))  # set all frefs values to f
+
+    return property(*(action.f[ref] for ref in action.f))
+
 
 action.qualname = None
 action.f = dict.fromkeys(["getter", "setter", "deleter", "documenter"], None)
 
-globals().update({fref: functools.partial(action, frefs=fref) for fref in {**action.f, 'getter_setter': None}})
-
+globals().update({fref: functools.partial(action, frefs=fref) for fref in {**action.f, "getter_setter": None}})
